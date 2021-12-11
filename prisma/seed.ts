aa1e4f6f45a0bdf6c9e9bae6f8affa3172eb51b3
Nathan Peek <nmp14fsu@gmail.com>
2021-12-11 01:17:22 -0500

    Create seeds for prisma and add email unique field

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { users } from "./user-seeds";
+
+const prisma = new PrismaClient();
+
+const seed = async (): Promise<void> => {
+    await prisma.user.deleteMany({});
+
+    for (const user of users) {
+        await prisma.user.create({
+            data: {
+                first_name: user.first_name,
+                middle_name: user.middle_name,
+                last_name: user.last_name,
+                password: await bcrypt.hashSync(user.password, 10),
+                email: user.email,
+                bio: user.bio
+            }
+        })
+    }
+}
+
+seed();
---
+++ prisma/user-seeds.ts
@@ -0,0 +1,24 @@
+export const users = [
+    {
+        "first_name": "Nathan",
+        "middle_name": "Morrill",
+        "last_name": "Peek",
+        "email": "testing@gmail.com",
+        "password": "testing",
+        "bio": "This is a bio"
+    },
+    {
+        "first_name": "Brit",
+        "middle_name": "d",
+        "last_name": "Smith",
+        "email": "testing2@gmail.com",
+        "password": "testing"
+    },
+    {
+        "first_name": "Leah",
+        "last_name": "Ferrell",
+        "email": "testing3@gmail.com",
+        "password": "testing",
+        "bio": "This is a bio2"
+    }
+]
